fe5b613158c34879410d3f3c8263d100e3f48fc1
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-01-03 20:14:26 +0200

    Add files via upload

---
+++ 20 Exam/22 October 2022/22October2022/01EnergyDrinks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01EnergyDrinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxCaffeine = 300;
+
+            Stack<int> caffeines = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse).ToList());
+          
+            Queue<int> drinks = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse).ToList());
+            int caffeineDey=0; 
+
+            while (caffeines.Count>0&&drinks.Count>0)
+            {
+                int caffeine=caffeines.Pop();
+                int drink=drinks.Dequeue();
+                if(caffeineDey+caffeine*drink<=maxCaffeine)
+                {
+                    caffeineDey+=drink*caffeine;
+                    continue;
+                }
+               
+                drinks.Enqueue(drink);
+                caffeineDey -= 30;
+                if (caffeineDey < 0)
+                {
+                    caffeineDey = 0;
+                }
+            }
+            if(drinks.Count>0)
+            {
+                string str = $"{string.Join(", ",drinks)}";
+                Console.WriteLine($"Drinks left: { str}");
+                Console.WriteLine($"Stamat is going to sleep with {caffeineDey} mg caffeine.");
+            }
+            else
+            {
+                Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+                Console.WriteLine($"Stamat is going to sleep with {caffeineDey} mg caffeine.");
+            }
+        }
+    }
+}
---
+++ 20 Exam/22 October 2022/22October2022/02RallyRacing/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+
+namespace _02RallyRacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+
+            string car = Console.ReadLine();
+            int carRow = 0;
+            int carCol = 0;
+            int distance = 0;
+
+            string[,] matrix = new string[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                string[] input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+           
+            bool finis = false;
+            string comand = Console.ReadLine();
+            while (comand != "End")
+            {
+                switch (comand)
+                {
+                  
+                    case "right":
+                        carCol++;
+                        break;
+                    case "left":
+                        carCol--;
+                        break;
+                    case "up":
+                        carRow--;
+                        break;
+                    case "down":
+                        carRow++;
+                        break;
+                }
+                if (matrix[carRow, carCol] == ".")
+                {
+                    distance += 10;
+                }
+                else if (matrix[carRow, carCol] == "T")
+                {
+                    distance += 30;
+                    matrix[carRow, carCol] = ".";
+                    for (int row = 0; row < n; row++)
+                    {
+                        for (int col = 0; col < n; col++)
+                        {
+                            if (matrix[row, col] == "T")
+                            {
+                                 carRow = row;
+                                 carCol = col;
+
+                                matrix[carRow, carCol] = ".";
+                            }
+                        }
+                    }
+
+                }
+                else if (matrix[carRow, carCol] == "F")
+                {
+                    distance += 10;
+
+                    finis = true;
+                    break;
+                }
+
+
+
+                comand = Console.ReadLine();
+
+            }
+
+            if (finis)
+            {
+                Console.WriteLine($"Racing car {car} finished the stage!");
+            }
+            else
+            {
+                Console.WriteLine($"Racing car {car} DNF.");
+            }
+            Console.WriteLine($"Distance covered {distance} km.");
+            matrix[carRow, carCol] = "C";
+            PrintCharMatrix(matrix);
+        }
+        private static void PrintCharMatrix(string[,] matrix)
+        {
+           
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
